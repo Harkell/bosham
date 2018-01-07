cb1f96e2299eabaec91c40e9f9e1bbda4d13e190
Jordan Harkell Short <jhs@jordans-mbp.home>
2018-01-07 16:30:43 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  root 'home#index'
+
+  get 'home/about'
+
+  get 'home/team'
+
+  get 'home/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
